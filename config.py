4809e42fe8f8abd107f361ee558f08742cc464a1
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-30 13:30:16 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather
+BOT_TOKEN = "1234567890:ABCabDEcdFeGH1IfJ2gKLhMijN_OklmPQR"
+
+# Project Name of your Project
+PROJECT_NAME = "GitInfo"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/madboy482/GitInfo"
